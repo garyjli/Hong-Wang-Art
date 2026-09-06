12753c3a20e86ee3ea4f2ba47d0832b0e5471701
garyjli <ligaryj1@gmail.com>
2026-09-06 00:43:12 -0400

    update header design
    - revert header design to original design
    - add hover effects to nav links

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,14 +3,14 @@ import Navigation from './Navigation'
 
 function Header() {
   return (
-    <header className='flex items-center justify-between pl-16 pr-24 py-5 shadow-md'>
+    <header className='flex flex-col gap-7 items-center pt-16 pb-4'>
       {/* "Hong Wang" Title */}
       <div className='p-0 m-0'>
         <Link to='/' className='no-underline'>
           <h3
             className={`
-              text-black text-[4rem] uppercase 
-              font-['Raleway',sans-serif] font-light p-0 m-0
+              text-black text-[4rem] uppercase transition-opacity duration-250 ease-[ease] 
+              font-['Raleway',sans-serif] font-light p-0 m-0 hover:opacity-50
             `}
           >
             Hong Wang
--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -7,7 +7,7 @@ import CartIcon from '../assets/cart.svg'
  * when the module loads instead of evaluating it on every render.
  */
 const textLink = `
-  text-black no-underline text-[1.7rem] font-['Raleway',sans-serif] font-light 
+  text-[1.6rem] font-['Raleway',sans-serif] font-light 
   transition-colors duration-250 ease-[ease] hover:text-[lightskyblue]
 `
 
@@ -15,38 +15,48 @@ function Navigation() {
   return (
     <div className="p-0 m-0">
       <nav>
-        <ul className="flex justify-center items-center list-none m-0 p-0 gap-14">
+        <ul className="flex justify-center items-center list-none m-0 p-0 gap-20">
           <li>
             <NavLink to="/">
               <img
-                className="w-10 h-10" 
+                className="w-9 h-9 transition-opacity duration-250 ease-[ease] hover:opacity-50"
                 src={HomeIcon}
                 alt='Home'
               />
             </NavLink>
           </li>
           <li>
-            <NavLink to="/gallery" className={textLink}>
+            <NavLink
+              to="/gallery"
+              className={({ isActive }) => 
+                `${textLink} ${isActive ? 'underline text-[lightskyblue]' : 'no-underline text-black'}`
+              }
+            >
               gallery
             </NavLink>
           </li>
           <li>
-            <NavLink to="/info" className={textLink}>
+            <NavLink
+              to="/info"
+              className={({ isActive }) => 
+                `${textLink} ${isActive ? 'underline text-[lightskyblue]' : 'no-underline text-black'}`
+              }
+            >
               info
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/cart">
-              <img
-                className="w-10 h-10" 
-                src={CartIcon}
-                alt='Cart'
-              />
             </NavLink>
           </li>
           <li>
             <NavLink to="/login" className={textLink}>
               login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart">
+              <img
+                className="w-9 h-9 transition-opacity duration-250 ease-[ease] hover:opacity-50"
+                src={CartIcon}
+                alt='Cart'
+              />
             </NavLink>
           </li>
         </ul>
