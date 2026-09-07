819a806960f9c8793bc6e776c591a375f31d8a6f
garyjli <ligaryj1@gmail.com>
2026-09-07 19:16:31 -0400

    change header reveal animation timing

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -14,7 +14,7 @@ const textLink = `
 function Navigation() {
   return (
     <div className="p-0 m-0 overflow-hidden">
-      <nav className="reveal-up" style={{ animationDelay: '200ms' }}>
+      <nav className="reveal-up" style={{ animationDelay: '150ms' }}>
         <ul className="flex justify-center items-center list-none m-0 p-0 gap-20">
           <li>
             <NavLink to="/">
